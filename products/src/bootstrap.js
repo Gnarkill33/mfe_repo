1bb22e399dff7dcd3f61c1c3b36f85f84061489c
Gnarkill33 <gnarkill33@gmail.com>
2026-08-27 23:14:48 +0300

    feat: add changes to products

---
+++ products/src/bootstrap.js
@@ -0,0 +1,20 @@
+import faker from "faker";
+
+export const mountProducts = (el) => {
+ let products = "";
+
+ for (let i = 0; i < 5; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+ }
+
+ el.innerHTML = products;
+};
+
+if (process.env.NODE_ENV === "development") {
+ const el = document.querySelector("#dev-products");
+
+ if (el) {
+  mountProducts(el);
+ }
+}
